61c510656a8072255f085bab3b1ae1edb8d5c89e
Your Name <you@example.com>
2025-03-28 19:18:18 -0300

    new try

--- list.c
+++ list.c
@@ -63,11 +63,15 @@ void * prevList(List * list) {
 }
 
 void pushFront(List * list, void * data) {
-    Node* nodo=createNode(data);
-    if(list->head) list->head->prev = nodo;
-    list->head = nodo;
-    nodo->next = list->head->next;
-    nodo->prev = NULL;
+    Node* nodo = createNode(data);
+    if (list->head == NULL) {
+        list->head = nodo;
+        list->tail = nodo;
+    } else {
+        nodo->next = list->head;
+        list->head->prev = nodo;
+        list->head = nodo;
+    }
 }
 
 void pushBack(List * list, void * data) {
